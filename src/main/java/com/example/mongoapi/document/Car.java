d00946ca8c3c027a8873ac55c316fe9565bfe643
GandyA23 <avila.gandy@gmail.com>
2021-02-21 15:27:41 -0600

    Document Car agregado

---
+++ src/main/java/com/example/mongoapi/document/Car.java
@@ -0,0 +1,20 @@
+package com.example.mongoapi.document;
+
+import com.mongodb.lang.NonNull;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document(collection = "cars")
+@Data
+public class Car implements Serializable {
+    @Id
+    @NonNull
+    private String _id ;
+    private String model ;
+    private String brand ;
+    private Integer year ;
+    private String color ;
+}
